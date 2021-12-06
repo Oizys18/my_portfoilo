2f016b160775b5e6eb60120cb0da3d68ce1131a9
Yang Chan Woo <oizys18@gmail.com>
2021-12-06 19:30:39 +0900

    Mod app, del font component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/provider'
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
-// import Fonts from '../lib/font'
 import '../styles/xterm.css'
 import React from 'react'
 import { appWithTranslation } from 'next-i18next'
@@ -9,7 +8,6 @@ import { appWithTranslation } from 'next-i18next'
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
-      {/* <Fonts /> */}
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
